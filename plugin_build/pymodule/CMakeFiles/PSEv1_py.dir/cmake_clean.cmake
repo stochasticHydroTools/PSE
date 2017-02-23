002f66b830f03157a5ac5ef9077246e9e8eac7e5
floren <fbusabiaga@gmail.com>
2017-02-22 22:00:38 -0500

    files from plugin_build deleted

--- plugin_build/pymodule/CMakeFiles/PSEv1_py.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/PSEv1_py"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/PSEv1_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
